fe254d51acd43ad219a10de270626307579183ad
AndreZanelato <andre.assis.zanelato@gmail.com>
2024-09-05 22:35:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Corrida de São Silvestre",
+        descricao: "Uma das corridas de rua mais tradicionais do Brasil, realizada anualmente no dia 31 de dezembro em São Paulo.",
+        link: "https://www.saosilvestre.com.br/"
+    },
+    {
+        titulo: "Maratona de Curitiba",
+        descricao: "Um dos principais eventos de corrida de rua do Sul do Brasil, oferecendo percursos de 42km, 21km, 10km e 5km.",
+        link: "https://maratonadecuritiba.com.br/"
+    },
+    {
+        titulo: "Rio de Janeiro 10K",
+        descricao: "A Maratona Rio é um dos eventos esportivos mais importantes do Brasil e da América Latina, reunindo milhares de corredores profissionais e amadores de diversas partes do mundo. Realizada anualmente na cidade do Rio de Janeiro, a prova oferece um percurso que convida os participantes a desfrutarem da beleza natural da cidade, com paisagens deslumbrantes que passam por pontos turísticos icônicos, como o Cristo Redentor e o Pão de Açúcar.",
+        link: "https://maratonadorio.com.br/"
+    }
+];
