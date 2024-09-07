c2c2518ec7988c646f4033da98b61468c636c6ff
AndreZanelato <andre.assis.zanelato@gmail.com>
2024-09-06 21:22:27 -0300

    refs - atualização do projeto

--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Corrida de São Silvestre",
         descricao: "Uma das corridas de rua mais tradicionais do Brasil, realizada anualmente no dia 31 de dezembro em São Paulo.",
-        link: "https://www.saosilvestre.com.br/"
+        link: "https://www.saosilvestre.com.br/",
+        tags: "silvestre, corrida, rua, dezembro, são paulo, sao paulo"
     },
     {
         titulo: "Maratona de Curitiba",
         descricao: "Um dos principais eventos de corrida de rua do Sul do Brasil, oferecendo percursos de 42km, 21km, 10km e 5km.",
-        link: "https://maratonadecuritiba.com.br/"
+        link: "https://maratonadecuritiba.com.br/",
+        tags: "corrida, rua, sul, curitiba, parana, paraná"
     },
     {
         titulo: "Rio de Janeiro 10K",
         descricao: "A Maratona Rio é um dos eventos esportivos mais importantes do Brasil e da América Latina, reunindo milhares de corredores profissionais e amadores de diversas partes do mundo. Realizada anualmente na cidade do Rio de Janeiro, a prova oferece um percurso que convida os participantes a desfrutarem da beleza natural da cidade, com paisagens deslumbrantes que passam por pontos turísticos icônicos, como o Cristo Redentor e o Pão de Açúcar.",
-        link: "https://maratonadorio.com.br/"
+        link: "https://maratonadorio.com.br/",
+        tags: "corrida, rio, cristo redentor, cristo, redentor, paisagem, america, américa, latina, américa latina, america latina"
     }
 ];
